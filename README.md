34fe19182dd85cb0be57861135364e8dbb46f581
WOOJINLEEdev <lwj3794@naver.com>
2022-05-08 15:48:51 +0900

    원티드 프리 온보딩 사전과제 컨버팅

--- README.md
+++ README.md
@@ -1,27 +1,2 @@
-# 원티드 프리 온보딩 프론트엔드 사전 과제
+# 원티드 프리온보딩 사전과제 컨버팅
 
-- React, Typescript, styled-components, react-icons 사용
-
-## Toggle
-
-- input checkbox를 이용하여 토글 구현
-
-## Tab
-
-- ul과 li, button 으로 탭 구현
-- active일 때 밑줄은 슬라이드 애니메이션으로 구현 (현재 탭 상태를 props로 전달)
-- 밑줄 슬라이드 애니메이션이 생각보다 쉽지 않았습니다.
-- ::before로 시도하다가 잘 되지 않아 div로 밑줄 영역을 따로 만들어 슬라이드 애니메이션을 구현하였습니다.
-
-## Slider
-
-- input range를 이용하여 구현
-
-## Input
-
-- 정규식으로 이메일 형식 체크
-- 비밀번호 보기/숨기기는 클릭 시 input type이 바뀌도록 구현
-
-## Dropdown
-
-- 검색은 filter를 이용하여 구현
--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,16 @@
-import "App.css";
-import Toggle from "components/Toggle";
-import Tab from "components/Tab";
-import Slider from "components/Slider";
-import Input from "components/Input";
-import Dropdown from "components/Dropdown";
-import styled from "styled-components";
-
 function App() {
   return (
-    <Container className="App">
-      <h1 className="title">
-        원티드 프리온보딩 프론트엔드 과제
-        <span className="sub_title">
-          (Toggle, Tab, Slider, Input, Dropdown)
-        </span>
-      </h1>
-      <Toggle />
-      <Tab />
-      <Slider />
-      <Input />
-      <Dropdown />
-    </Container>
-  );
+    <div className='app'>
+      <header className='appHeader'>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className='appLink' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
-export default App;
-
-const Container = styled.main`
-  min-height: 100vh;
-  padding: 20px 0 100px;
-
-  .title {
-    width: 600px;
-    text-align: center;
-    font-size: 35px;
-    padding: 10px 0;
-    font-weight: bold;
-    margin: 0 auto;
-
-    .sub_title {
-      font-size: 20px;
-    }
-  }
-`;
+export default App
